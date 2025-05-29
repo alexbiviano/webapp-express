541432eef34e40566a9987adf608dbeb0ced70e6
alexbiviano <bivianoalex63@gmail.com>
2025-05-29 15:12:30 +0200

    [upd] instaurate rotte

--- app.js
+++ app.js
@@ -9,3 +9,53 @@ const db = mysql.createConnection({
     password: '12345678',
     database: 'db_movie'
 }) 
+
+db.connect((err) => {  
+    if (err) {  
+        console.error('Errore di connessione: ' + err.stack);  
+        return;  
+    }  
+    console.log('Connesso al database MySQL come ID ' + db.threadId);  
+});  
+
+app.get('/', (req, res) => {  
+    res.send('Hello World!');  
+});  
+
+app.get('/movies', (req, res) => {  
+    db.query('SELECT * FROM movies', (err, results) => {  
+        if (err) {  
+            return res.status(500).send(err);  
+        }  
+        res.json(results);   
+    });  
+});  
+
+ 
+app.get('/movies/:id', (req, res) => {  
+    const movieId = req.params.id;  
+
+
+    db.query('SELECT * FROM movies WHERE id = ?', [movieId], (err, movieResults) => {  
+        if (err) {  
+            return res.status(500).send(err);  
+        }  
+        if (movieResults.length === 0) {  
+            return res.status(404).send('Film non trovato');  
+        }  
+
+        db.query('SELECT * FROM reviews WHERE movie_id = ?', [movieId], (err, reviewResults) => {  
+            if (err) {  
+                return res.status(500).send(err);  
+            }  
+            res.json({  
+                movie: movieResults[0],  
+                reviews: reviewResults  
+            });  
+        });  
+    });  
+});  
+
+app.listen(PORT, () => {  
+    console.log(`Server is running on http://localhost:${PORT}`);  
+});  
